213923dfcc4029f9cd539376fa1863b4beea36be
AbdulazizQM <abdulazizqutb@gmail.com>
2025-02-18 22:53:55 +0200

    tt

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,9 +78,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD': 'yqtCQhtupwWXcUgVJiQeuaUEkxcdmOuR',
+        'HOST':'turntable.proxy.rlwy.net',
+        'PORT':'56650',
+        }
 }
 
 
