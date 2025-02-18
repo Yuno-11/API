900b38caa56e019e65f5166903dccdca8f196cb4
AbdulazizQM <abdulazizqutb@gmail.com>
2025-02-19 00:28:25 +0200

    ;

--- myapp/serializer.py
+++ myapp/serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import modelpredict
 
 class predictserializer(serializers.ModelSerializer):
+    image = serializers.ImageField()
+
     class Meta:
         model = modelpredict
         fields = ['pk', 'image', 'predict_class', 'predict_accuracy', 'predicted']
