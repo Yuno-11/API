d93f82cf15be0772a654a9f4a5fcd78708f6c888
FaresNawar <143773055+FaresNawar@users.noreply.github.com>
2025-03-28 03:50:07 +0200

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -11,7 +11,7 @@ class modelpredict(models.Model):
 class esp32_data(models.Model):
     device_id = models.CharField(max_length=100, unique=True)
     email = models.EmailField(blank=True, null=True)
-    image = models.ImageField(upload_to='esp32_images/',default="")
+    image = models.TextField(max_length=100000000000000000) # ImageField(upload_to='esp32_images/',default="")
     temperature = models.FloatField()
     humidity = models.FloatField()
     predict_class = models.CharField(max_length=50, blank=True, null=True)
